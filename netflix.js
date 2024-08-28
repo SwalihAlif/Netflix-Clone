bdfea93d0c8d7da155e7a92b855367ba195be46d
SwalihAlif <zzray485@gmail.com>
2024-08-28 13:20:21 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,5 @@
+var globeIcon = document.getElementById('globeIcon');
+var Hglobe = document.getElementsByClassName('Hglobe')[0];
+
+globeIcon.style.filter = 'invert(100%)';
+Hglobe.style.filter = 'invert(100%)';
